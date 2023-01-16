5f6dde33a9983fd1bdd6741c641f15f1263770d8
agata-zybula <agata.masiewicz@gmail.com>
2023-01-16 13:09:41 +0100

    hm8 edited

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,80 +1,29 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form');
-const email = document.querySelector('input[name=email]');
-const message = document.querySelector('textarea[name-message]');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formEmail = document.querySelector('input[name=email]');
+const formMessage = document.querySelector('textarea[name=message]');
 
-
-// window.addEventListener('load', () => {
-//   Array.from(form.elements).forEach(el => {
-//     if (el.name) {
-//       const prevValue = localStorage.getItem(el.name);
-//       el.value = prevValue;
-//     }
-//   });
-// });
-
-// const onInput = form.addEventListener('input', e => {
-//   e.preventDefault();
-//   Array.from(form.elements).forEach(el => {
-//     if (el.name) {
-//       localStorage.setItem(el.name, el.value);
-//     }
-//   });
-// });
-
-// form.addEventListener('submit', e => {
-//   Array.from(form.elements).forEach(el => {
-//     if (el.name) {
-//       localStorage.removeItem(el.name, el.value);
-//        console.log({
-//          email: form.elements.email.value,
-//          message: form.elements.message.value,
-//        });
-//     }
-//   });
-// });
-
-// form.addEventListener("input", throttle(onInput, 500));
-
-
-window.addEventListener("load", () => { //automatyczne wypelnianie formualrza po wejsciu na strone (zapamietanie danych)
-  Array.from(form.elements).forEach(el => {
-    if (el.name) {
-      const prevVal = localStorage.getItem(el.name); //prevVal = email         prevVal = message
-      el.value = prevVal; // test@test.pl = email     wiadomoscktorajestwpisana = message
-    }
-  });
+window.addEventListener('load', () => { //automatyczne wypelnianie formualrza po wejsciu na strone (zapamietanie danych) - jesli nie klikne submit (czyli niewyslane dane)
+  const prevVal = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localStorage.getItem('feedback-form-state') !== null) {
+    formEmail.value = prevVal.email;
+    formMessage.value = prevVal.message;
+  }
 });
 
-
-// form.addEventListener('input', e => { //zdarzenie input - po uzupelnieniu formularza, ale NIE WYSLANIU, dane sa zapisane w localstorage i po przeladowaniu strony nadal tam beda - automatycznie sie uzupelnia
-//   e.preventDefault();
-//   Array.from(form.elements).forEach(el => {
-//     if (el.name) {
-//       localStorage.setItem(el.name, el.value);
-//     }
-//   });
-// });
-
 form.addEventListener('input', throttle(() => {
-  Array.from(form.elements).forEach(el => {
-    if (el.name) {
-      localStorage.setItem(el.name, el.value);
-    }
-  })
-}, 500));
-
+    let someData = {
+      email: formEmail.value,
+      message: formMessage.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(someData));
+  }, 500)
+);
 
 form.addEventListener('submit', e => { //jak klikne submit, to wyczysci sie formularz i localstorage, po przeladowaniu strony bede miec pusty formularz, bo localstorage zostalo wyczyszczone
   e.preventDefault();
-  Array.from(form.elements).forEach(el => {
-    if (el.name) {
-      localStorage.setItem(el.name, el.value);
-    }
-  });
-  form.reset();
-  localStorage.clear();
-  }
-);
+  console.log(localStorage.getItem('feedback-form-state')); //obiekt z danymi w konsoli
+  form.reset(); //czyszczenie formularza po submit
+  localStorage.clear(); //czyszczenie localStorage po submit
+});
